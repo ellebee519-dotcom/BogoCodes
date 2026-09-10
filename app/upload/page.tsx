e09111551ee87f03cdc7429b712aae47d545fa83
ellebee519 <ellebee519@gmail.com>
2026-09-10 05:27:49 -0500

    fix export in upload page component

---
+++ app/upload/page.tsx
@@ -0,0 +1,8 @@
+export default function UploadPage() {
+  return (
+    <div style={{ padding: "50px", fontFamily: "sans-serif", textAlign: "center" }}>
+      <h1>Upload Page</h1>
+      <p>This page is currently under maintenance.</p>
+    </div>
+  );
+}
