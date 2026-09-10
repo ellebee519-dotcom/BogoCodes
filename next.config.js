72945269e9940df950a3e140b012895b1358f0ed
ellebee519 <ellebee519@gmail.com>
2026-09-10 04:37:53 -0500

    add ignore build errors to next config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@ const nextConfig = {
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
   },
   allowedDevOrigins: ['100.115.92.198', '100.115.92.198:3000'],
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
